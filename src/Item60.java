5e3086617f7413b03fecf90292e163f1fa298304
yatw <wongyatman487@gmail.com>
2022-02-06 09:58:16 -0800

    59, 60

---
+++ src/Item60.java
@@ -0,0 +1,12 @@
+
+public class Item60 {
+
+    public static void main(String[] args){
+
+        System.out.println(1.03 - 0.42); // print 0.6100000000000001, NOT 0.61
+        System.out.println(1.00 -9 * 0.10); // print 0.09999999999999998, NOT 0.1
+
+    }
+
+
+}
